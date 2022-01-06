fa3f2f23c276f3fd37f3255ff59e58d217cabf65
zouritre <timouf97400@live.fr>
2022-01-06 13:30:00 +0100

    Initial Commit

---
+++ teki/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  teki
+//
+//  Created by Bertrand Dalleau on 06/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
