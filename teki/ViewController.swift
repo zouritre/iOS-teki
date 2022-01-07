f592e29fb6576e7b88cf3ba72f8dfa3e3297e292
zouritre <timouf97400@live.fr>
2022-01-08 00:35:28 +0100

    Add random texte generation, end of the project.

--- teki/ViewController.swift
+++ teki/ViewController.swift
@@ -8,9 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var activities = ["du dancefloor", "du barbecue", "de la surprise ratée", "des blagues lourdes", "de la raclette party"]
+    var celebrities = ["le Steve Jobs", "le Zinedine Zidane", "la Madonna", "le Karl Lagarfeld", "la Scarlett Johansson"]
     @IBOutlet weak var quoteLabel: UILabel!
     @IBAction func changeQuote() {
-        quoteLabel.text = "Texte modifié"
+        quoteLabel.text = "Tu es " + celebrities[Int(arc4random_uniform(UInt32(celebrities.count-1)))] + " " +
+        activities[Int(arc4random_uniform(UInt32(activities.count-1)))]
     }
     
 }
