689d7079a9994bea0e19bf8839ea04d2fcd78235
zouritre <timouf97400@live.fr>
2022-01-07 22:49:58 +0100

    Add test logic to main button  in controller

--- teki/ViewController.swift
+++ teki/ViewController.swift
@@ -8,12 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var quoteLabel: UILabel!
+    @IBAction func changeQuote() {
+        print("Youpie")
     }
-
-
+    
 }
 
